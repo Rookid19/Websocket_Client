91801b3a9df4b4b4d95c5fb2783b4a996bc83207
Eric Aidoo <ericaidoo@Erics-MacBook-Pro.local>
2022-04-09 16:36:16 -0400

    socket client

---
+++ client.js
@@ -0,0 +1,28 @@
+const WebSocket = require('ws');
+
+//server addy
+const serverUrl = 'ws://127.0.0.1:3004';
+
+const ws = new WebSocket(serverUrl);
+
+
+ws.on('open', function() {
+    const data = {
+            "id": "acct_1293ijwjw",
+            "firstName": "Charles",
+            "lastName": "Darwin",
+            "company": "Tesla, Inc.",
+            "ticker": "TSLA",
+            "gain": "$200.57"
+        }
+
+    ws.send('Hey server, I just connected: ' + JSON.stringify(data.gain));
+});
+
+ws.on('message', function(msg) {
+   
+    console.log('Message form the server: ' + msg);
+
+})
+
+
