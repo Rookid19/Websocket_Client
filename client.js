9a231f3e4b46754f4054a7d0a841684a16d450bb
roodev <randyodoom19@gmail.com>
2022-04-21 19:04:50 +0000

    websocket

--- client.js
+++ client.js
@@ -1,27 +1,41 @@
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 
-const serverUrl = 'ws://127.0.0.1:3004';
+const serverUrl = "ws://localhost:3004";
 
 const ws = new WebSocket(serverUrl);
 
+ws.on("open", function () {
+   const users = [
+      {
+         id: "acct_273wbuwbu",
+         firstName: "Grey",
+         lastName: "Hennesay",
+         price: 10,
+         shares: 5,
+      },
+      {
+         id: "acct_2537839j3hewjw",
+         firstName: "Rae",
+         lastName: "Morgan",
+         price: 20,
+         shares: 10,
+      },
+   ];
 
-ws.on('open', function() {
-    const data = {
-            "id": "acct_1293ijwjw",
-            "firstName": "Charles",
-            "lastName": "Darwin",
-            "company": "Tesla, Inc.",
-            "ticker": "TSLA",
-            "gain": "$200.57"
-        }
-
-    ws.send('Hey server, I just connected: ' + JSON.stringify(data.gain));
+   setInterval(() => {
+      let stockPrice = Math.floor(Math.random() * 798);
+      // console.log(a);
+      for (i in users) {
+         let val = (users[i].price + stockPrice) * users[i].shares;
+         // ws.send(JSON.stringify(i + " index ----> " + val));
+         // ws.send({
+         //    "message",
+         //    val
+         // })
+      }
+   }, 30000);
 });
 
-ws.on('message', function(msg) {
-   
-    console.log('Message form the server: ' + msg);
-
-})
-
-
+ws.on("message", function (msg) {
+   console.log("Message form the server: " + msg);
+});
