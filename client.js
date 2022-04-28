5712cecf69383091ae1558b849adaf826898d881
roodev <randyodoom19@gmail.com>
2022-04-28 08:52:30 +0000

    firebase

--- client.js
+++ client.js
@@ -1,7 +1,6 @@
+const { addDoc, doc, getFirestore, collection } = require("firebase/firestore");
 const WebSocket = require("ws");
-const { auth, db } = require("./firebase");
-
-
+const { db } = require("./firebase");
 
 const serverUrl = "ws://localhost:3004";
 
@@ -25,18 +24,21 @@ ws.on("open", function () {
       },
    ];
 
-   setInterval(() => {
+   // setInterval(() => {
       // console.log(a);
       for (let i in users) {
          // let val = (users[i].price + stockPrice) * users[i].shares;
          // ws.binaryType = "arraybuffer";
-         ws.send(JSON.stringify(users[i]));
+         addDoc(collection(db, "UserInfo", "randyodoom@gmail.com","text"), {
+            id: 1,
+         });
+         // ws.send(JSON.stringify(users[i]));
          // ws.send({
          //    "message",
          //    val
          // })
       }
-   }, 30000);
+   // }, 3000);
 });
 
 ws.on("message", function (msg) {
