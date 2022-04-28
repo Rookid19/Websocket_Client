62358d691127da53752d4ed5b9e0c627b1ac34bd
roodev <randyodoom19@gmail.com>
2022-04-28 10:06:32 +0000

    graph

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 const { addDoc, doc, getFirestore, collection } = require("firebase/firestore");
 const WebSocket = require("ws");
+const { test } = require("./data");
 const { db } = require("./firebase");
 
 const serverUrl = "ws://localhost:3004";
@@ -7,62 +8,21 @@ const serverUrl = "ws://localhost:3004";
 const ws = new WebSocket(serverUrl);
 
 ws.on("open", function () {
-   const users = [
-      {
-         id: "acct_273wbuwbu",
-         firstName: "Grey",
-         lastName: "Hennesay",
-         price: 130,
-         shares: 0.129347,
-         email: "test@123.com",
-      },
-      {
-         id: "acct_2537839j3hewjw",
-         firstName: "Rae",
-         lastName: "Morgan",
-         price: 60,
-         shares: 0.2916,
-         email: "test@yahoo.com",
-      },
-      {
-         id: "acct_2537839j3hewjw",
-         firstName: "Rae",
-         lastName: "Moo",
-         price: 60,
-         shares: 0.2916,
-         email: "test@gmail.com",
-      },
-      {
-         id: "acct_356679j3hewjw",
-         firstName: "Rae",
-         lastName: "Aban",
-         price: 60,
-         shares: 0.2916,
-         email: "test@uk.com",
-      },
-      {
-         id: "acct_265463hewjw",
-         firstName: "Joe",
-         lastName: "Poyle",
-         price: 200,
-         shares: 0.7916,
-         email: "test@us.com",
-      },
-   ];
 
-   setInterval(() => {
+
+   // setInterval(() => {
       // console.log(a);
       for (let i in users) {
          // let val = (users[i].price + stockPrice) * users[i].shares;
          // ws.binaryType = "arraybuffer";
-
+test
          ws.send(JSON.stringify(users[i]));
          // ws.send({
          //    "message",
          //    val
          // })
       }
-   }, 60000);
+   // }, 60000);
 });
 
 ws.on("message", function (msg) {
--- data.js
+++ data.js
@@ -1,24 +1,53 @@
-export default users = [
-   {
-      id: "acct_273wbuwbu",
-      firstName: "Grey",
-      lastName: "Hennesay",
-      holdings: {
-         company: "Amazon, Inc",
-         ticker: "AMZN",
-         gain: "$327.89",
-         timestamp: "2692262672",
-      },
-   },
-   {
-      id: "acct_2537839j3hewjw",
-      firstName: "Rae",
-      lastName: "Morgan",
-      holdings: {
-         comppany: "Tesla, Inc.",
-         ticker: "TSLA",
-         gain: "-$55.90",
-         timestamp: "27822667787",
-      },
-   },
-];
+const { query, collection, getDocs } = require("firebase/firestore");
+const { db } = require("./firebase");
+
+const emails = [];
+exports.emails = emails;
+export async function test() {
+   const q = query(collection(db, "UserInfo"));
+   const querySnapshot = await getDocs(q);
+   emails.push(doc.data().email);
+}
+
+// const users = [
+//    {
+//       id: "acct_273wbuwbu",
+//       firstName: "Grey",
+//       lastName: "Hennesay",
+//       price: 130,
+//       shares: 0.129347,
+//       email: "test@123.com",
+//    },
+//    {
+//       id: "acct_2537839j3hewjw",
+//       firstName: "Rae",
+//       lastName: "Morgan",
+//       price: 60,
+//       shares: 0.2916,
+//       email: "test@yahoo.com",
+//    },
+//    {
+//       id: "acct_2537839j3hewjw",
+//       firstName: "Rae",
+//       lastName: "Moo",
+//       price: 60,
+//       shares: 0.2916,
+//       email: "test@gmail.com",
+//    },
+//    {
+//       id: "acct_356679j3hewjw",
+//       firstName: "Rae",
+//       lastName: "Aban",
+//       price: 60,
+//       shares: 0.2916,
+//       email: "test@uk.com",
+//    },
+//    {
+//       id: "acct_265463hewjw",
+//       firstName: "Joe",
+//       lastName: "Poyle",
+//       price: 200,
+//       shares: 0.7916,
+//       email: "test@us.com",
+//    },
+// ];
